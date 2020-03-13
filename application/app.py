d125e18330254d24fb0d51a4255c88ccf8aaa4ba
Kieran7741 <kieran7741@gmail.com>
2020-03-13 15:47:44 +0000

    added flask-sessions to allow for larger cookie to be stored server side

--- application/app.py
+++ application/app.py
@@ -1,15 +1,23 @@
 from flask import Flask, jsonify, request, render_template, session
 from flask_bootstrap import Bootstrap
-from twitter.twitter_api import get_api, get_multiple_tweets, TweetLite
+from twitter.twitter_api import get_api, get_multiple_tweets
 from twitter_keys import CONSUMER_KEY, CONSUMER_SECRET, API_KEY, SECRET_KEY
 from database.db_ops import get_db, get_tweets_from_collection
+from flask_session import Session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['SESSION_USE_SIGNER'] = False
 app.config['SECRET_KEY'] = CONSUMER_SECRET
+app.config['SESSION_TYPE'] = 'mongodb'
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = (timedelta(minutes=10))
+
 Bootstrap(app)
+Session(app)
+
 twitter_api = get_api(CONSUMER_KEY, CONSUMER_SECRET, API_KEY, SECRET_KEY)
 db = get_db('tweets')
-import json
 
 
 @app.route('/positive')
@@ -27,11 +35,9 @@ def find_tweet():
 def display_tweets():
     session['topic'] = request.form['topic']
     session['num_tweets'] = int(request.form['num_tweets'])
-    tweets = get_multiple_tweets(twitter_api, session['topic'], session['num_tweets'], tweetlite=True)
-    print(tweets)
-    session['tweets'] = tweets
+    session['tweets'] = get_multiple_tweets(twitter_api, session['topic'], session['num_tweets'], tweetlite=True)
     return render_template('classify.html', topic=session['topic'], num_tweets=session['num_tweets'],
-                           tweet=session['tweets'][session['num_tweets'] -1])
+                           tweet=session['tweets'][session['num_tweets'] - 1])
 
 
 @app.route('/classify/<tweet_id>', methods=['POST'])
@@ -41,16 +47,15 @@ def classify_tweet(tweet_id):
     :param tweet_id: Id of tweet
     :return:
     """
-    print(session)
     session['num_tweets'] = session['num_tweets'] - 1
-    # Implement logic for updating tweet sentiment in the database
-    # set_tweet_sentiment(tweet_id, request.form['sent'])
+
     print('Tweet ID:', tweet_id, 'Sentiment', request.form['sent'])
     db.manually_classified.insert_one(session['tweets'][session['num_tweets']])
     if session['num_tweets']:
         return render_template('classify.html', topic=session['topic'], num_tweets=session['num_tweets'],
                                tweet=session['tweets'][session['num_tweets'] - 1])
     else:
+        # Clear out the session
         session.pop('num_tweets')
         session.pop('topic')
         return render_template('search.html')
