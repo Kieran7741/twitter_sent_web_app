430cce6f6ffa1adcaa5a6fc2a36bfa2ff793fa77
Kieran7741 <kieran7741@gmail.com>
2020-04-24 20:51:01 +0100

    added doc strings and images

--- create_sent_model.py
+++ create_sent_model.py
@@ -17,6 +17,13 @@ import re
 
 
 def load_tweets(db, collection):
+    """
+    Load tweets from database
+    :param db: Authenticated mongodb instance
+    :param collection: Collection of tweets
+    :return: List of tweets
+    :rtype: list of dict
+    """
     positive_tweets = get_tweets_from_collection(db, collection, {'sentiment': 'positive'})
     neutral_tweets = get_tweets_from_collection(db, 'manually_classified', {'sentiment': 'neutral'})
     negative_tweets = get_tweets_from_collection(db, 'manually_classified', {'sentiment': 'negative'})
@@ -27,11 +34,17 @@ def load_tweets(db, collection):
 def process_tweet_text(tweets):
     """
     Remove twitter related meta data from tweets, usernames for example
-    :param tweets: list of tweets
+    :param tweets: list of tweets to process
     :return: list of tweets with updated text
+    :rtype: list of dict
     """
 
     def regex(text_):
+        """
+        Sanitize provided text
+        :param text_: Text to clean
+        :return: Cleaned up text
+        """
         # Remove http links
         text = re.sub(r'http\S+', '', text_)
         # Remove username
@@ -49,13 +62,13 @@ def process_tweet_text(tweets):
     return tweets
 
 
-def compare_sentiment(x_test, y_test, custom_model_socre):
+def compare_sentiment(x_test, y_test, custom_model_score):
     """
-    Compare vader sentiment to model sentiment
-    :param x_test:
-    :param y_test:
-    :param custom_model_socre:
-    :return:
+    Compare vader sentiment to model sentiment. Vader does not require training data
+    :param x_test: Tweets to test predict sentiment of.
+    :param y_test: Actual sentiment
+    :param custom_model_score: Score from custom model
+    :return: Vaders predictions and accuracy score
     """
     vader_map = {'neg': 'negative', 'pos': 'positive', 'neu': 'neutral'}
     vader_predictions = []
@@ -68,53 +81,53 @@ def compare_sentiment(x_test, y_test, custom_model_socre):
 
     vader_score = accuracy_score(y_test, vader_predictions)
     print(f'Vader scored: {vader_score}')
-    print(f'Custom model scored: {custom_model_socre}')
+    print(f'Custom model scored: {custom_model_score}')
 
     cm = confusion_matrix(y_test, vader_predictions, labels=['positive', 'neutral', 'negative'], normalize='true')
     plot = ConfusionMatrixDisplay(cm, ['positive', 'neutral', 'negative'])
-    plot.plot(xticks_rotation=90)
+    plot.plot(xticks_rotation=45)
     plot.ax_.set_title(f'Vader Score: {vader_score}')
     plot.figure_.canvas.set_window_title('Vader Confusion Matrix')
     plt.subplots_adjust(bottom=0.25, right=0.80, top=0.75)
     return vader_predictions, vader_score
 
 
-api = get_api(CONSUMER_KEY, CONSUMER_SECRET, API_KEY, SECRET_KEY)
-db = get_db('tweets')
+if __name__ == "__main__":
 
-all_tweets = process_tweet_text(load_tweets(db, 'manually_classified'))
+    api = get_api(CONSUMER_KEY, CONSUMER_SECRET, API_KEY, SECRET_KEY)
+    db = get_db('tweets')
 
-tweet_df = pd.DataFrame(columns=['text', 'sentiment'])
-for tweet in all_tweets:
-    tweet_df.loc[len(tweet_df)] = [tweet['text'], tweet['sentiment']]
+    all_tweets = process_tweet_text(load_tweets(db, 'manually_classified'))
 
-x_train, x_test, y_train, y_test = train_test_split(tweet_df.text,  tweet_df.sentiment, shuffle=True, random_state=7)
+    tweet_df = pd.DataFrame(columns=['text', 'sentiment'])
+    for tweet in all_tweets:
+        tweet_df.loc[len(tweet_df)] = [tweet['text'], tweet['sentiment']]
 
-# It is important to reuse the same vectorizer for new data also
-tfid_vectorizer = TfidfVectorizer(max_df=0.5, stop_words=stopwords.words('english'), tokenizer=process_text,
-                                  max_features=10000)
-print('Fitting train data')
-x_train_vector = tfid_vectorizer.fit_transform(x_train)
-x_test_vector = tfid_vectorizer.transform(x_test)
+    x_train, x_test, y_train, y_test = train_test_split(tweet_df.text,  tweet_df.sentiment, shuffle=True, random_state=7)
 
-print(f'Number of features: {len(tfid_vectorizer.get_feature_names())}')
+    # It is important to reuse the same vectorizer for new data also
+    tfid_vectorizer = TfidfVectorizer(max_df=0.5, stop_words=stopwords.words('english'), tokenizer=process_text,
+                                      max_features=10000)
+    print('Fitting train data')
+    x_train_vector = tfid_vectorizer.fit_transform(x_train)
+    x_test_vector = tfid_vectorizer.transform(x_test)
 
-model = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=500, random_state=7)
-model.fit(x_train_vector, y_train)
+    print(f'Number of features: {len(tfid_vectorizer.get_feature_names())}')
 
-predictions = model.predict(x_test_vector)
-print(predictions)
-score = accuracy_score(y_test, predictions)
-print(score)
+    model = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=500, random_state=7)
+    model.fit(x_train_vector, y_train)
 
-result_string = f'{model}: Score: {round(score, 3)}'
+    predictions = model.predict(x_test_vector)
+    score = accuracy_score(y_test, predictions)
+    result_string = f'{model}: Score: {round(score, 3)}'
 
-cm = confusion_matrix(y_test, predictions, labels=['positive', 'neutral', 'negative'], normalize='true')
-plot = ConfusionMatrixDisplay(cm, ['positive', 'neutral', 'negative'])
-plot.plot(xticks_rotation=90)
-plot.ax_.set_title(result_string)
-# plot.figure_.canvas.set_window_title('Custom Model Confusion Matrix')
-plt.subplots_adjust(bottom=0.25, right=0.80, top=0.75)
-compare_sentiment(x_test, y_test, score)
-plt.show()
+    # Plot confusion matrix of custom model
+    cm = confusion_matrix(y_test, predictions, labels=['positive', 'neutral', 'negative'], normalize='true')
+    plot = ConfusionMatrixDisplay(cm, ['positive', 'neutral', 'negative'])
+    plot.plot(xticks_rotation=45)
+    plot.ax_.set_title(result_string)
+    plot.figure_.canvas.set_window_title('Custom Model Confusion Matrix')
+    plt.subplots_adjust(bottom=0.25, right=0.80, top=0.75)
 
+    compare_sentiment(x_test, y_test, score)
+    plt.show()
